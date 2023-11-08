2bc77d95d4cee721e5dd7ad87d285c35f0fdc4ce
Rupam <rupammondal564@email.com>
2023-11-08 15:39:49 +0530

    question 4 complete

---
+++ intro-js-operation/Assignment2/question4.js
@@ -0,0 +1,5 @@
+// declaring varibale is very importantin any programming language because it allocated storage
+// in our system . Without declaration if we use variables that will create compilation issue
+
+let a = 18
+console.log(a);
