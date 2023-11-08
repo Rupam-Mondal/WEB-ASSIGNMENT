52211ad11d791ac8566a23396ca315ae8ed5b989
Rupam <rupammondal564@email.com>
2023-11-08 15:32:20 +0530

    complete question 6 of assign 2

---
+++ intro-js-operation/Assignment2/question6.js
@@ -0,0 +1,9 @@
+// We should always give meaning full variable name in the code. It increases readability
+// of the code. Suppose i have to store age in a variable but i gave variable to name "firstname"
+// then obiously it will create problem in reading the code.
+
+// *It will not create any issue in user side but to make understand other developer we should
+// follow the rules 
+
+let age = 18
+console.log(age);
