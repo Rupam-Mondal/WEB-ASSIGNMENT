0633c3c644967b6798bb8ff03fd87a470744fa19
Rupam <rupammondal564@email.com>
2023-12-02 08:35:58 +0530

    function question2 is done

---
+++ function_js/question2.js
@@ -0,0 +1,7 @@
+function generateGreeting(name){
+    return `${name}, Welcome to our website`
+}
+
+console.log(generateGreeting("Rupam"));
+console.log(generateGreeting("Abhi"));
+console.log(generateGreeting("Anuj"));
