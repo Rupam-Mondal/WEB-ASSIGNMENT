78f4c56826f1af350f5a61e0f84c8701ccef5b75
Rupam <rupammondal564@email.com>
2023-12-03 18:59:27 +0530

    question4 is done

---
+++ function_js/question4.js
@@ -0,0 +1,20 @@
+function calculateTax(income) {
+    if(income >= 20000 && income <= 50000){
+        return function calculate() {
+            const ans = (income * 3)/100;
+            console.log("Your tax is = " , ans);
+        };
+    }
+    else if(income > 50000 && income <= 90000){
+        return function calculate() {
+            const ans = (income * 5)/100;
+            console.log("Your tax is = " , ans);
+        };
+    }
+    return function notax(income) {
+        console.log("No tax is needed");
+    }
+}
+
+const ans = calculateTax(71000)
+ans();
