059e372175e3abfa4ddd496f6bcae0de2d4f3ba0
Rupam <rupammondal564@email.com>
2023-12-03 19:00:50 +0530

    update

--- function_js/question4.js
+++ function_js/question4.js
@@ -3,13 +3,13 @@ function calculateTax(income) {
         return function calculate() {
             const ans = (income * 3)/100;
             console.log("Your tax is = " , ans);
-        };
+        }
     }
     else if(income > 50000 && income <= 90000){
         return function calculate() {
             const ans = (income * 5)/100;
             console.log("Your tax is = " , ans);
-        };
+        }
     }
     return function notax(income) {
         console.log("No tax is needed");
